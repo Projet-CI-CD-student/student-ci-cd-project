3dd34756445e30c48163c174ef6f2d61022346dd
cronosjl <jeanddjl@gmail.com>
2026-01-15 16:35:41 +0100

    Fix:  Build Docker et IP 0.0.0.0

--- app/src/main.ts
+++ app/src/main.ts
@@ -6,17 +6,13 @@ import HttpException from './app/models/http-exception.model';
 
 const app = express();
 
-/**
- * App Configuration
- */
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Sécurité pour l'import des routes (Fix Build Prod)
+// SÉCURITÉ IMPORT (Build Prod Fix)
 app.use((routes as any).default ? (routes as any).default : routes);
 
-// Serves images
 app.use(express.static(__dirname + '/assets'));
 
 app.get('/', (req: express.Request, res: express.Response) => {
@@ -31,25 +27,26 @@ app.use(
     res: express.Response,
     next: express.NextFunction,
   ) => {
+    // @ts-ignore
     if (err && err.name === 'UnauthorizedError') {
       return res.status(401).json({
         status: 'error',
         message: 'missing authorization credentials',
       });
-    } else if (err && (err as HttpException).errorCode) {
-      res.status((err as HttpException).errorCode).json(err.message);
+      // @ts-ignore
+    } else if (err && err.errorCode) {
+      // @ts-ignore
+      res.status(err.errorCode).json(err.message);
     } else if (err) {
       res.status(500).json(err.message);
     }
   },
 );
 
-/**
- * Server activation
- */
 const PORT = process.env.PORT || 3000;
 
-// FIX : Utilisation de '0.0.0.0' pour l'accessibilité Docker
+// --- LA CORRECTION CRITIQUE EST ICI ---
+// On force l'écoute sur 0.0.0.0 sinon Docker bloque la connexion entrante
 app.listen(Number(PORT), '0.0.0.0', () => {
   console.info(`server up on port ${PORT}`);
 });
